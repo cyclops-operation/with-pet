d02d73df7c3eaaf657818cb16341b1ccd21b247b
p-acid <qkrtkstjd01@gmail.com>
2023-01-09 21:45:27 +0900

    setting(storybook): Add storybook

---
+++ components/Button/index.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import Button, { ButtonProps } from '.'
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    color: {
+      control: { type: 'select' }
+    },
+    size: {
+      control: { type: 'select' }
+    }
+  }
+} as ComponentMeta<typeof Button>
+
+export const Default: ComponentStory<typeof Button> = (props: ButtonProps) => (
+  <Button {...props}>Button</Button>
+)
