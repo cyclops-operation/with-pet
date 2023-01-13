2412a848e8d758e205cb1a0b1a6836871da15cbd
Samuel Park <87939521+p-acid@users.noreply.github.com>
2023-01-14 00:45:48 +0900

    feat(radio): Add Radio component (#5)

---
+++ components/Radio/index.stories.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import Radio, { RadioProps } from '.'
+
+export default {
+  title: 'Components/Radio',
+  component: Radio,
+  argTypes: {
+    value: {
+      control: 'text'
+    },
+    size: {
+      control: 'select'
+    }
+  }
+} as ComponentMeta<typeof Radio>
+
+export const Default: ComponentStory<typeof Radio> = (props: RadioProps) => (
+  <Radio {...props}>Radio</Radio>
+)
