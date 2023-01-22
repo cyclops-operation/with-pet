3adc04b67fc2c0b69f454373118696471bb21356
Genie <jeonbg@kakao.com>
2023-01-22 23:30:16 +0900

    fix(input): 불필요한 코드 제거

--- pages/find/index.page.tsx
+++ pages/find/index.page.tsx
@@ -1,13 +1,7 @@
-import InputBox from '~/components/Input/input-box'
-
 const Find = () => {
   return (
     <div>
-      <InputBox className='flex max-w-[320px] flex-col gap-2'>
-        <InputBox.Label text='Email' htmlFor='input-2' />
-        <InputBox.Input id='input-2' placeholder='olivia@untitledui.com' />
-        <InputBox.HintText text='This is a hint text to help user.' />
-      </InputBox>
+      <span>Find</span>
     </div>
   )
 }
