637a6f8f21b3da2ee8e55eceba38a269c18ba817
p-acid <qkrtkstjd01@gmail.com>
2023-02-03 00:20:02 +0900

    fix(axios): change axios api depth

--- api/axios.ts
+++ api/axios.ts
@@ -5,14 +5,18 @@ import { API_ENCODE_KEY } from '~/common/env'
 import { getQueryString } from '~/common/utils'
 import { GetCityParams, GetCityResponse } from '~/interfaces/axios'
 
-export const Location = {
-  getCity: async (params: GetCityParams) => {
-    const queryString = getQueryString({ ...params })
+const Api = {
+  Location: {
+    getCity: async (params: GetCityParams) => {
+      const queryString = getQueryString({ ...params })
 
-    const { data } = await api.get<AxiosResponse<GetCityResponse>>(
-      `/sido?serviceKey=${API_ENCODE_KEY}&${queryString}&_type=json`
-    )
+      const { data } = await api.get<AxiosResponse<GetCityResponse>>(
+        `/sido?serviceKey=${API_ENCODE_KEY}&${queryString}&_type=json`
+      )
 
-    return data
+      return data
+    }
   }
 }
+
+export default Api
